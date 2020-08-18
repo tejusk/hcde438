83c293d4b306373f8e28dacb721692dfd4c5b999
tejusk <tejusk@uw.edu>
2020-08-17 22:23:28 -0700

    Fixed messages.map

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -5,7 +5,7 @@ import Footer from './footer'
 import Message from './message'
 
 function App() {
-  const [messages, setMessages] = useState('')
+  const [messages, setMessages] = useState([])
   return (
     <main className = "main">
       <header>
@@ -14,8 +14,7 @@ function App() {
       </header>
 
       <div className = "messages">
-        {messages.map((m, i) => 
-          {return <Message key = {i} text = {m} />})}
+        {messages.map((m, i) => <Message key = {i} text = {m} />)}
       </div>
 
       <Footer
